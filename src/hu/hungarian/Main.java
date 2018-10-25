8c52132d131e3c05ac45aeaac226829f58c4f1fd
madsmansour <42993941+madsmansour@users.noreply.github.com>
2018-10-25 10:41:45 +0200

    løst opgave

--- src/hu/hungarian/Main.java
+++ src/hu/hungarian/Main.java
@@ -7,8 +7,20 @@ import java.lang.reflect.Field;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
         SmartLight s = new SmartLight();
         System.out.println(s.toString());
+
+
+        SmartLight mads1 = new SmartLight();
+        mads1.setColor(Color.BLACK);
+        mads1.setLightPercent((short) 20);
+        System.out.println(s.toString());
+
+        SmartLight mads2 = new SmartLight();
+        mads2.setColor(Color.RED);
+        mads2.setLightPercent((short) (20));
+        System.out.println(s.toString());
+
+
     }
 }
--- src/hu/hungarian/SmartLight.java
+++ src/hu/hungarian/SmartLight.java
@@ -4,6 +4,49 @@ import javafx.scene.paint.Color;
 
 public class SmartLight {
 
+    private short lightPercent;
+    private Color color;
 
+    public SmartLight() {
+        color = Color.WHITESMOKE;
+        lightPercent = 0;
+    }
+    public void turnUp(){
+        lightPercent += 20;
+        if (lightPercent > 100)
+            lightPercent = 100;
+    }
 
+    public void turnDown(){
+        lightPercent -= 20;
+        if (lightPercent < 0)
+            lightPercent = 0;
+
+    }
+    public String toString(){
+        return "The color of the smartlight is: " + getColor() + "And the lightpercent is at: " + getLightPercent();
+    }
+
+    public short getLightPercent() {
+        return lightPercent;
+    }
+
+    public void setLightPercent(short lightPercent) {
+        if (lightPercent > 100 )
+            lightPercent = 100;
+        if (lightPercent < 0)
+            lightPercent = 0;
+        this.lightPercent = lightPercent;
+
+    }
+
+    public Color getColor() {
+        if (color == Color.BLACK);
+        color = color.WHITESMOKE;
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
